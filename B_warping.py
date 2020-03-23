5421c61d109bab34e5ac793f7144691b9d8f5a1b
ChristineGra <chrisosch@gmx.de>
2020-03-23 12:03:44 +0100

    Save plots for other data

--- B_warping.py
+++ B_warping.py
@@ -12,7 +12,7 @@ from affinewarp.crossval import heldout_transform
 # load dataset to be analysed
 path_datasets_folder = "datasets"
 # [spike position, trial number, spikeID]
-dataset = np.load(os.path.join(path_datasets_folder, "dataset_ec013.752_769.5_part0.npy"))
+dataset = np.load(os.path.join(path_datasets_folder, "dataset_ec013.752_769.5_part1.npy"))
 trials = [int(x) for x in dataset[1]]
 spike_positions = [int(x) for x in dataset[0]]
 spike_IDs = [int(x) for x in dataset[2]]
@@ -49,11 +49,9 @@ def plot_column(axes, spike_data):
         ax.scatter(
             spike_data.spiketimes[spike_data.neurons == n],
             spike_data.trials[spike_data.neurons == n],
-            **raster_kws,
+            **raster_kws
         )
-        ax.set_xlabel("position")
-        ax.set_ylabel("trial")
-
+        
 
 shift_model = ShiftWarping(
     maxlag=MAXLAG,
@@ -86,12 +84,12 @@ pwise3_model = PiecewiseWarping(
     smoothness_reg_scale=SMOOTHNESS_REG_SCALE,
 )
 
-models = [shift_model, linear_model, pwise1_model, pwise2_model, pwise3_model]
+models = [shift_model, linear_model, pwise1_model, pwise2_model]
 
 neurons_to_plot = [i for i in range(12) if spike_IDs.count(i) > 5]
 print("neurons to plot: ", neurons_to_plot)
 
-for model, label in zip(models, ('shift', 'linear', 'pwise-1', 'pwise-2', 'pwise-3')):
+for model, label in zip(models, ('shift', 'linear', 'pwise-1', 'pwise-2')):
 
     # Fit and apply warping to held out neurons.
     validated_alignments = heldout_transform(
@@ -102,7 +100,7 @@ for model, label in zip(models, ('shift', 'linear', 'pwise-1', 'pwise-2', 'pwise
 
     # Create figure.
 
-    fig, axes = plt.subplots(len(neurons_to_plot), 3, figsize=(9.5, 6))
+    fig, axes = plt.subplots(len(neurons_to_plot), 2, figsize=(9.5, 6))
 
     # First column, raw data.
     plot_column(
@@ -110,29 +108,38 @@ for model, label in zip(models, ('shift', 'linear', 'pwise-1', 'pwise-2', 'pwise
     )
 
     # Second column, re-sorted trials by warping function.
-    plot_column(
-        axes[:, 1],
-        data.reorder_trials(model.argsort_warps())
-    )
+    # plot_column(
+    #     axes[:, 1],
+    #     data.reorder_trials(model.argsort_warps())
+    # )
 
     # Third column, shifted alignment.
     plot_column(
-        axes[:, 2],
+        axes[:, 1],
         validated_alignments
     )
 
-    fig.suptitle("Dataset 0 (mouse runs from left to right)")
+    fig.suptitle("Dataset 1 (mouse runs from left to right)")
     axes[0, 0].set_title("raw data")
-    axes[0, 1].set_title("sorted by warp (" + label + " warp)")
-    axes[0, 2].set_title("aligned by model (" + label + " warp)")
+    # axes[0, 1].set_title("sorted by warp (" + label + " warp)")
+    axes[0, 1].set_title("aligned by model (" + label + " warp)")
 
+    for ax in axes[:, 1]:
+        ax.set_ylabel("trial")
+    for  ax in axes[-1, :]:
+        ax.set_xlabel("position")
+            
     for index, axis in enumerate(axes[:, 0]):
         axis.set_ylabel("n. " + str(neurons_to_plot[index]))
     fig.tight_layout()
     fig.subplots_adjust(hspace=.9, top=0.9)
 
-# TODO save plots
-plt.show(block=True)
+    # TODO save plots
+    path_plots_folder = "plots"
+    plt.savefig(os.path.join(path_plots_folder, "Bdata", label + "1"))
+                
+# plt.show(block=True)
 
 # line to add conda to path: export PATH=$PATH:/storage2/perentos/code/python/conda/anaconda/bin
 # https://askubuntu.com/questions/186808/every-command-fails-with-command-not-found-after-changing-bash-profile
+
