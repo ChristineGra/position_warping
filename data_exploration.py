d004a0e2a5e72ced8fb20c92a0b1ef1860ba4674
ChristineGra <chrisosch@gmx.de>
2020-03-25 17:17:06 +0100

    nice plots

--- data_exploration.py
+++ data_exploration.py
@@ -12,11 +12,15 @@ import pprint
 
 path_to_data = '/storage2/perentos/data/recordings/NP46/NP46_2019-12-02_18-47-02/processed/place_cell_tunings.mat'
 
-def plot_data(data, neuronID, axis):
+def plot_data(data, neuronID, axis, category):
         axis.imshow(np.squeeze(data))
         FR = np.mean(data, axis=0)
         axis.plot(FR, c='r')
         axis.set_ylim(bottom=0, top=60)
+
+        title = str(neuronID)[2:-2] + " " + category
+        axis.set_title(title, fontsize=6)
+        axis.set_xticks([100])
     
 
     
@@ -77,51 +81,66 @@ def load_spikedata(path):
         print("selected trials shape: ", selected_trials.shape)
 
         # create model
-        shift_model = ShiftWarping(maxlag=.3, smoothness_reg_scale=8.)
-        linear_model = PiecewiseWarping(n_knots=0, warp_reg_scale=0, smoothness_reg_scale=8.)
-        p1_model = PiecewiseWarping(n_knots=1, warp_reg_scale=0.5, smoothness_reg_scale=3.)
-        p2_model = PiecewiseWarping(n_knots=3, warp_reg_scale=0, smoothness_reg_scale=3.)
-        model = p1_model
-        
-        # fit model
-        model.fit(selected_trials, iterations=30)
-        # transform data based on fitted model
-        data_transformed = model.transform(selected_trials)
+        shift_model = ShiftWarping(maxlag=.3, smoothness_reg_scale=5., warp_reg_scale=0.3)
+        linear_model = PiecewiseWarping(n_knots=0, warp_reg_scale=0.3, smoothness_reg_scale=5.)
+        p1_model = PiecewiseWarping(n_knots=1, warp_reg_scale=0.3, smoothness_reg_scale=5.)
+        p2_model = PiecewiseWarping(n_knots=2, warp_reg_scale=0.3, smoothness_reg_scale=5.)
+        models = [shift_model, linear_model, p1_model, p2_model]
+        labels = ["shift", "linear", "piecewise-1", "piecewise-2"]
         
         # filter neuron IDs so that only selected neurons are taken into account
         neurons = neuronIDs[selected_neurons[0]]
-        
+
         slices = [i for i in range(len(neurons)) if i%12 == 0]
         slices.append(len(neurons) + 1)
         print(slices)
 
-        for slice1, slice2 in zip(slices[:-1], slices[1:]):
-            neurons_to_plot = neurons[slice1: slice2]
-            print(neurons_to_plot)
-            limit = math.ceil(len(neurons_to_plot)/2)
-            print(limit)
-            fig, axes = plt.subplots(limit, 4, figsize=(9.5, 6))
-            
-            # select neuron
-            for neuronID, index in zip(neurons_to_plot, range(len(neurons_to_plot))):
-                if index >= limit:
-                    ax_raw = axes[index-limit, 2]
-                    ax_transformed = axes[index-limit, 3]
-                else:
-                    ax_raw = axes[index, 0]
-                    ax_transformed = axes[index, 1]
-                    
-                # select data
-                raw_data = selected_trials[:, :, index]
-                raw_data = np.expand_dims(raw_data, -1)
+        
+        for model, label in zip(models, labels):
+            # fit model
+            model.fit(selected_trials, iterations=30)
+            # transform data based on fitted model
+            data_transformed = model.transform(selected_trials)
 
-                # plot data before warping with FR
-                plot_data(raw_data, neuronID, ax_raw)
+            for slice1, slice2 in zip(slices[:-1], slices[1:]):
+                neurons_to_plot = neurons[slice1: slice2]
+                print(neurons_to_plot)
+                limit = math.ceil(len(neurons_to_plot)/2)
+                print(limit)
+                fig, axes = plt.subplots(limit, 4, figsize=(9.5, 6))
+                fig.subplots_adjust(hspace=.4)
+                fig.suptitle("Data for " + label + " warp")
+                axes[0, 0].set_title("raw data")
+                axes[0, 2].set_title("raw data")
+                axes[0, 1].set_title("aligned by model")
+                axes[0, 3].set_title("aligned by model")
 
-                # plot transformed data with FR
-                transformed_data = data_transformed[:, :, index]
-                plot_data(transformed_data, neuronID, ax_transformed)
-        plt.show(block=True)
+                for ax in axes[:, 0]:
+                    ax.set_ylabel("trial")
+                for  ax in axes[-1, :]:
+                    ax.set_xlabel("position")
+
+
+                # select neuron
+                for neuronID, index in zip(neurons_to_plot, range(len(neurons_to_plot))):
+                    if index >= limit:
+                        ax_raw = axes[index-limit, 2]
+                        ax_transformed = axes[index-limit, 3]
+                    else:
+                        ax_raw = axes[index, 0]
+                        ax_transformed = axes[index, 1]
+
+                    # select data
+                    raw_data = selected_trials[:, :, index]
+                    raw_data = np.expand_dims(raw_data, -1)
+
+                    # plot data before warping with FR
+                    plot_data(raw_data, neuronID, ax_raw, "raw")
+
+                    # plot transformed data with FR
+                    transformed_data = data_transformed[:, :, index]
+                    plot_data(transformed_data, neuronID, ax_transformed, "aligned")
+                plt.savefig(os.path.join("plots", label, "neuron" + str(slice1) + "-" + str(slice2) + str(label) + "_maxlag_warpreg03_smooth5"))
 
 
 
